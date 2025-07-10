904dbcad416d8703f3170a846fcdb35baaf8811d
Andrei Ioniță <hi@andrei.io>
2025-07-10 14:39:21 +0300

    fix default falback locale

--- config/app.php
+++ config/app.php
@@ -82,7 +82,7 @@ return [
 
     'locale' => env('APP_LOCALE', 'ro'),
 
-    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'ro'),
+    'fallback_locale' => env('APP_FALLBACK_LOCALE', 'en'),
 
     'faker_locale' => env('APP_FAKER_LOCALE', 'en_US'),
 
