7c39e0244f149c391004614db8ff9cb98810131f
Lupu Gheorghe <gheorghelupu17@gmail.com>
2025-07-11 11:42:58 +0300

    wip

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,14 +1,14 @@
 <x-layouts.app>
-    <div class="py-16 md:py-24 ">
+    <div class="pt-16 md:pt-12  pb-10 md:pb-10">
         <div class="max-w-4xl px-6 mx-auto  lg:px-8">
             <h1 class="text-2xl md:text-4xl font-semibold tracking-tight text-gray-900 text-center text-balance">
                 Declarații de avere și interese
             </h1>
-            <p class="mt-8 text-lg font-medium text-gray-500 text-pretty ">
-                Platforma Declarații de avere este un proiect care își propune să digitalizeze întreaga arhivă de declarații de avere ale politicienilor aleși și a funcționarilor publici din România. De la anunțul făcut de Curtea Constituțională a României pe 29 mai 2025 prin care a anulat caracterul public al declarațiilor de avere și de interese, <a href="https://www.code4.ro/ro" target="_blank" class=" underline hover:text-primary-600 ">Code for Romania</a> alături de inițiativa civică <a href="https://primulrand.ro/" target="_blank"  class=" underline hover:text-primary-600 " >În Primul Rând</a> a lucrat pentru a salva cât mai mult posibil din arhivele Agenției Naționale de Integritate și pentru a ne asigura că accesul la informațiile istorice privind declarațiile de avere și interese rămân publice si transparente.
+            <p class="mt-8 text-md font-medium text-gray-500 text-pretty ">
+                Platforma Declarații de avere și interese este un proiect care își propune să digitalizeze întreaga arhivă de declarații de avere și interese ale politicienilor aleși și a funcționarilor publici din România. De la anunțul făcut de Curtea Constituțională a României pe 29 mai 2025 prin care a anulat caracterul public al declarațiilor de avere și de interese, <a href="https://www.code4.ro/ro" target="_blank" class=" underline hover:text-primary-600 ">Code for Romania</a> alături de inițiativa civică <a href="https://primulrand.ro/" target="_blank"  class=" underline hover:text-primary-600 " >În Primul Rând</a> a lucrat pentru a salva cât mai mult posibil din arhivele Agenției Naționale de Integritate și pentru a ne asigura că accesul la informațiile istorice privind declarațiile de avere și interese rămân publice si transparente.
             </p>
-            <p class="mt-8 text-lg font-medium text-gray-500 text-pretty ">
-                În acest portal vom publica, într-un format accesibil informațiile istorice referitoare la aceste documente. Digitalizarea este un proces de durată și suntem în plin proces de prelucrare a celor peste 12 milioane de declarații disponibile. De asemenea, uneori, în ciuda mecanismelor de extragere și validare de informații se pot strecura greșeli. Dacă sesizați orice eroare pe această platformă, vă suntem recunoscători dacă ne puteți transmite un mesaj urmând acest <a href="mailto://contact@code4.ro" target="_blank" class="underline hover:text-primary-400">link</a>.
+            <p class="mt-8 text-md font-medium text-gray-500 text-pretty ">
+                În acest portal vom publica, într-un format accesibil informațiile istorice referitoare la aceste documente. Digitalizarea este un proces de durată și suntem în plin proces de prelucrare a celor peste 12 milioane de declarații disponibile. De asemenea, uneori, în ciuda mecanismelor de extragere și validare de informații se pot strecura greșeli. Dacă sesizezi orice eroare pe această platformă, transmite-ne mesaj urmând acest <a href="mailto:contact@code4.ro" target="_blank" class="underline hover:text-primary-400">link</a>.
             </p>
         </div>
     </div>
